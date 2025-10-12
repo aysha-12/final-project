269b122ae4dcfef0fe992999224382879421ff73
aysha-12 <munniaysha555@gmail.com>
2025-10-12 14:46:07 +0600

    change path name

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Home from './Page/Home/Home';
 import BlankPage from './Page/Share/BlanckPage';
 import Product from './firebase/product/Product';
 import Share from './Page/Share/Share';
-import Signup from './Page/create-account/Signup';
+import Signup from './Page/create-account/SignUp';
 import Login from './Page/create-account/Loging';
 import Blog from './Page/Blogs/Blog';
 import PrivateRoute from './Page/PrivateRoute/PrivateRoute';
